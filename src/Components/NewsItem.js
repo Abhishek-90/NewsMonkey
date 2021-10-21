c6309f9a2eac4ad785a1c7de434e6af83edfea39
Abhishek <holaniabhishek90@gmail.com>
2021-10-21 15:49:17 +0530

    Static news Items rendering

--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -1,21 +1,27 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export class NewsItem extends Component {
-    render() {
-        let {title, description} = this.props;
-        return (
-            <div>
-                <div class="card" style={{width: "18rem"}}>
-                <img src="..." class="card-img-top" alt="Photo related to news"/>
-                <div class="card-body">
-                    <h5 class="card-title">{title}</h5>
-                    <p class="card-text">{description}</p>
-                    <a href="/newsDetails" class="btn btn-primary">Go somewhere</a>
-                </div>
-                </div>
-            </div>
-        )
-    }
+  render() {
+    let { title, description,urlToImage,newsUrl} = this.props;
+    return (
+      <div>
+        <div className="card" style={{ width: "18rem" }}>
+          <img
+            src={urlToImage}
+            className="card-img-top"
+            alt="related to news"
+          />
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text" he>{description}</p>
+            <a href={newsUrl} target="_blank" className="btn btn-primary">
+              Continue Reading
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default NewsItem
+export default NewsItem;
